3819f606d7f1961ff3eb37bd0eebdd1129e1d25a
Hungquestionnaire <ivy062984@gmail.com>
2025-03-25 13:48:17 +0800

    questionnaire url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/L53x5mPsrw8yfp4w7',
+	'https://forms.gle/qp43tzKchzh6fqsd8',
+	'https://forms.gle/RCiYKJ9Hmp9A4zSq8',
+	'https://forms.gle/X2j2m2njkjWzUuTG9',
+	'https://forms.gle/P7LkambAG1ifrsJH7',
+	'https://forms.gle/5kaoLL7npHQN4GKz6',
+	'https://forms.gle/JR1XEyTU8iNNAo8T7',
+	'https://forms.gle/oyGHc9Q2ZB2sWJmp6',
 ];
