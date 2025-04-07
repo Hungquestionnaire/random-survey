7489123a11564d69299c41e9c142962a77ef2b71
Hungquestionnaire <ivy062984@gmail.com>
2025-04-07 23:50:04 +0800

    survey1 url.js

--- url.js
+++ url.js
@@ -8,7 +8,12 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/mLvAKPnMGfhWe4Wu7',
+	'https://forms.gle/kLrF7vDpiEZD4jxF8',
+	'https://forms.gle/TsfbcbVgwe1cXvP48',
+	'https://forms.gle/uh88QdYpRjAEJLJY9',
+	'https://forms.gle/zH2js6JwcoTxnPxp9',
+	'https://forms.gle/ZGJmETr4U2jn6S7k9',
+	'https://forms.gle/HSUA2YCgZqUpAF6u5',
+	'https://forms.gle/17rCCzX11LR2pHEY7',
 ];
